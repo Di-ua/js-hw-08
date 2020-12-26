37a09985985f909db6b9fe100e0da6570fd60337
Diana Kuzmina <diana.kuzmina@users.noreply.github.com>
2020-12-26 08:46:35 +0200

    Add script and styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import items from "./gallery-items";
+import galleryItems from "./gallery-items.js";
 
 /*********Задание
 Создай галерею с возможностью клика по ее элементам и просмотра полноразмерного изображения в модальном окне. Превью результата посмотри по ссылке.
@@ -28,3 +28,96 @@
   </a>
 </li>
  */
+// const galleryListRef = document.querySelector("ul.js-gallery");
+// galleryListRef.addEventListener("click", onGalleryClick);
+
+// const createItem = (item, array) => {
+//   const itemRef = document.createElement("li");
+//   const linkRef = document.createElement("a");
+//   const imageRef = document.createElement("img");
+//   const { preview, original, description } = item;
+//   itemRef.classList.add("gallery__item");
+// };
+const galleryContainer = document.querySelector(".js-gallery");
+const lightboxWindow = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".lightbox__image");
+const lightboxOverlay = document.querySelector(".lightbox__overlay");
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+lightboxOverlay.addEventListener("click", onBackdropClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+              <a class="gallery__link" href=${original}>
+                <img
+                  class="gallery__image"
+                  src=${preview}
+                  data-source=${original}
+                  alt=${description}
+                />
+              </a>
+            </li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  lightboxWindow.classList.add("is-open");
+  openImage(event);
+  window.addEventListener("keydown", onKeyPress);
+  closeModalBtn.addEventListener("click", closeModal);
+}
+
+function openImage(event) {
+  lightboxImage.src = event.target.dataset.source;
+  lightboxImage.alt = event.target.alt;
+}
+
+function closeModal() {
+  lightboxWindow.classList.remove("is-open");
+  lightboxImage.src = "";
+  lightboxImage.alt = "";
+  window.removeEventListener("keydown", onKeyPress);
+  closeModalBtn.removeEventListener("click", closeModal);
+}
+
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+}
+
+function onKeyPress(event) {
+  const indexArray = galleryItems.map((item) => item.original);
+  let index = indexArray.indexOf(lightboxImage.src);
+  switch (event.code) {
+    case "Escape":
+      closeModal();
+      break;
+    case "ArrowLeft": {
+      if (index === 0) {
+        index = galleryItems.length;
+      }
+      lightboxImage.src = galleryItems[index - 1].original;
+      break;
+    }
+    case "ArrowRight": {
+      if (index === galleryItems.length - 1) {
+        index = -1;
+      }
+      lightboxImage.src = galleryItems[index + 1].original;
+      break;
+    }
+  }
+}
